e36fd67835d8e34fed54745704f1c9920fbba3f4
Pierre Paquin <p.paquin1016@laposte.net>
2023-08-15 11:54:42 +0200

    fix: centre la vue a l'ajout du marker

--- src/components/map/google-maps.component.tsx
+++ src/components/map/google-maps.component.tsx
@@ -8,14 +8,15 @@ const GoogleMapsComponent = () => {
     const { isLoaded } = useLoadScript({
         googleMapsApiKey: config.API_KEY_GOOGLE_MAPS,
     });
+    const metz: google.maps.LatLngLiteral = { lat: 49.133333, lng: 6.166667 }
+    const [center, setCenter] = useState(metz)
+    const [markers, setMarker] = useState([center]);
 
-    const metz = { lat: 49.133333, lng: 6.166667 }
-    const [markers, setMarker] = useState([metz]);
-
-    const onClick = (e: google.maps.MapMouseEvent) => {
+    const onClickAddMarker = (e: google.maps.MapMouseEvent) => {
         if (e.latLng !== null) {
-            const addingMark = {lat: e.latLng.lat(), lng: e.latLng.lng()}
+            const addingMark: google.maps.LatLngLiteral = {lat: e.latLng.lat(), lng: e.latLng.lng()}
             setMarker([...markers, addingMark]);
+            setCenter(addingMark);
         }
     }
 
@@ -24,15 +25,20 @@ const GoogleMapsComponent = () => {
             {!isLoaded ? (
                 <LoaderMapComponent></LoaderMapComponent>
             ) : (
-                <GoogleMap
-                    mapContainerClassName="map-container"
-                    center={metz}
-                    zoom={10}
-                    onClick={onClick}
-                >
-                    <MarkersViewComponent markers={markers}></MarkersViewComponent>
-                </GoogleMap>
+                    <GoogleMap
+                        mapContainerClassName="map-container"
+                        center={center}
+                        zoom={10}
+                        onClick={onClickAddMarker}
+                    >
+                        <MarkersViewComponent markers={markers}></MarkersViewComponent>
+                    </GoogleMap>
             )}
+            {!isLoaded ? (
+                <div></div>
+            ) : (<div>
+                <button>LOL</button>
+            </div>)}
         </div>
     );
 };
